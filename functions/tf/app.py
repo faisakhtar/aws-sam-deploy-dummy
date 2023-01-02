65db3f30e1476ef15d57c69238d1cb0bac9e9602
faisakhtar <faisakhtar@gmail.com>
2023-01-02 18:23:13 +0000

    Redeploy lambda

--- functions/tf/app.py
+++ functions/tf/app.py
@@ -3,9 +3,9 @@ import boto3
 
 def lambda_handler(event, context):
     
-   print("Lambda function deployed via terraform 2...")
+   print("Lambda function deployed via terraform 3...")
    return {
         'statusCode': 200,
-        'body': json.dumps('Lambda function 2 successful')
+        'body': json.dumps('Lambda function 3 successful')
    }
     
