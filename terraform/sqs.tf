5d9756559a89704afa3331a0ed83bf2cb6a5b244
faisakhtar <faisakhtar@gmail.com>
2023-01-02 22:10:38 +0000

    Add new sqs

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -10,3 +10,16 @@ resource "aws_sqs_queue" "terraform_queue" {
     name = "terraform-example-queue"
   }
 }
+
+resource "aws_sqs_queue" "terraform_queue2" {
+  name                      = "terraform-example-queue2"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Env = "dev"
+    name = "terraform-example-queue2"
+  }
+}
