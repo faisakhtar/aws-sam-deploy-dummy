96ed5751082536dbb5db8fdc7bbcbc8f66b42aab
faisakhtar <faisakhtar@gmail.com>
2023-01-02 18:13:25 +0000

    Lambda updates

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,21 +18,20 @@ resource "aws_lambda_function" "sampletf" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
-  
+
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = <<EOF
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
         }
+      },
     ]
-    }
-    EOF
+  })
 }
