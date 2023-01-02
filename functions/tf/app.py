0783b4ce14fbf47189f10c5465902a388c9a18c1
faisakhtar <faisakhtar@gmail.com>
2023-01-02 17:56:12 +0000

    Update lambda tf

--- functions/tf/app.py
+++ functions/tf/app.py
@@ -3,9 +3,9 @@ import boto3
 
 def lambda_handler(event, context):
     
-   print("Lambda function deployed via terraform...")
+   print("Lambda function deployed via terraform 2...")
    return {
         'statusCode': 200,
-        'body': json.dumps('Lambda function successful')
+        'body': json.dumps('Lambda function 2 successful')
    }
     
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -18,20 +18,21 @@ resource "aws_lambda_function" "sampletf" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
-
+  ignore_changes = [name, arn]
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "lambda.amazonaws.com"
+  assume_role_policy = <<EOF
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
         }
-      },
     ]
-  })
+    }
+    EOF
 }
